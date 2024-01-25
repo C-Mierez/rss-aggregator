47b8122956506d22019d953dc98a810545924a63
C-Mierez <carlos.mierez20@gmail.com>
2024-01-25 18:51:39 -0300

    ✨ Graceful shutdown wrapper

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,16 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/c-mierez/rss-aggregator/internal/env"
 	"github.com/c-mierez/rss-aggregator/internal/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func init() {
@@ -18,10 +22,11 @@ func init() {
 }
 
 func main() {
+	// Create a global context
+	globalCtx, globalCtxCancel := context.WithCancel(context.Background())
 
 	// Connect to database
-	db := setUpDatabase()
-	defer db.Close(context.Background())
+	db := setUpDatabase(globalCtx)
 
 	// Create a new router
 	router := setUpRouter()
@@ -32,15 +37,63 @@ func main() {
 		Addr:    "127.0.0.1:" + env.Get(env.PORT),
 	}
 
-	log.Printf("Starting server on PORT: %+v\n", env.Get(env.PORT))
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatalf("Could not start server: %s\n", err.Error())
-	}
+	gracefulShutdownWrapper(globalCtx, func() {
+		log.Printf("Starting server on PORT: %+v\n", env.Get(env.PORT))
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Could not start server: %s\n", err.Error())
+		}
+	}, func(shutdownCtx context.Context) {
+		// Shutdown the server
+		if err := server.Shutdown(shutdownCtx); err != nil {
+			log.Fatalf("Could not gracefully shutdown server: %s\n", err.Error())
+		}
+
+		// Close the database connection
+		db.Close()
+
+		log.Println("Graceful shutdown complete.")
+
+		// Cancel the global context
+		globalCtxCancel()
+	})
+
+	<-globalCtx.Done() // Wait for the global context to be cancelled
 
 }
 
-func setUpDatabase() *pgx.Conn {
-	db, err := pgx.Connect(context.Background(), env.Get(env.DATABASE_URL))
+func gracefulShutdownWrapper(globalCtx context.Context, gracefulExecution func(), gracefulShutdown func(shutdownCtx context.Context)) {
+
+	// Graceful shutdown signal channel
+	shutdownChan := make(chan os.Signal, 1)
+	signal.Notify(shutdownChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	// Routine to handle graceful shutdown
+	go func() {
+		sig := <-shutdownChan
+		log.Printf("Received signal %s. Shutting down...\n", sig.String())
+
+		// Create a failsafe trigger to shut down after 10 seconds if the graceful shutdown does not finish in time
+		failsafeCtx, failsafeCtxCancel := context.WithTimeout(globalCtx, 10*time.Second)
+		defer failsafeCtxCancel()
+
+		go func() {
+			<-failsafeCtx.Done() // Wait for the failsafe context to be cancelled after it times out
+
+			if failsafeCtx.Err() == context.DeadlineExceeded {
+				log.Fatal("Graceful shutdown timed out. Shutting down immediately...")
+			}
+		}()
+
+		// Perform all the graceful shutdown tasks
+		gracefulShutdown(failsafeCtx)
+	}()
+
+	// Routine in which the graceful execution will take place
+	go gracefulExecution()
+}
+
+func setUpDatabase(ctx context.Context) *pgxpool.Pool {
+	db, err := pgxpool.New(ctx, env.Get(env.DATABASE_URL))
 	if err != nil {
 		log.Fatalf("Could not connect to database: %s\n", err.Error())
 	}
