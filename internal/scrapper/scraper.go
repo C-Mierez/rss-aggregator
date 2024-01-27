8749001a47d9d1047c5e1e2a79cb7026e84f12a3
C-Mierez <carlos.mierez20@gmail.com>
2024-01-27 17:43:50 -0300

    ✨ Improved network usage by preventing scraping before expiration time

--- internal/scrapper/scraper.go
+++ internal/scrapper/scraper.go
@@ -4,11 +4,17 @@ import (
 	"context"
 	"database/sql"
 	"log"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/c-mierez/rss-aggregator/internal/lib/queries"
 	"github.com/c-mierez/rss-aggregator/internal/lib/rss"
+	"github.com/google/uuid"
+)
+
+const (
+	expiredFeedThreshold = 2 * time.Minute
 )
 
 func StartScraping(
@@ -35,10 +41,19 @@ func StartScraping(
 			continue
 		}
 
+		// Filter all feeds that are within the expiredFeedThreshold
+		var expiredFeeds []queries.Feed
+		for _, feed := range feeds {
+			if feed.LastFetchedAt.Valid && time.Since(feed.LastFetchedAt.Time) < expiredFeedThreshold {
+				continue
+			}
+			expiredFeeds = append(expiredFeeds, feed)
+		}
+
 		// Create a wait group to wait for all goroutines to finish
 		wg := &sync.WaitGroup{}
 
-		for _, feed := range feeds {
+		for _, feed := range expiredFeeds {
 			wg.Add(1)
 
 			go scrapeFeed(wg, db, feed)
@@ -69,7 +84,33 @@ func scrapeFeed(wg *sync.WaitGroup, db *queries.Queries, feed queries.Feed) {
 	}
 
 	for _, item := range fetchedFeed.Channel.Item {
-		log.Printf("Found Item: %+v", item)
+		log.Printf("Found Item: %+v", item.Title)
+		// Parse Publication Date
+		// 	TODO: This should be made more robust to support all types of date formats
+		parsedPubDate, err := time.Parse(time.RFC1123Z, item.PubDate)
+		if err != nil {
+			log.Printf("Error parsing publication date: %s", err.Error())
+			continue
+		}
+		// Insert item
+		_, err = db.CreatePost(context.Background(), queries.CreatePostParams{
+			ID:          uuid.New(),
+			Title:       item.Title,
+			Description: item.Description,
+			Url:         item.Link,
+			PublishedAt: parsedPubDate,
+			CreatedAt:   time.Now().UTC(),
+			UpdatedAt:   time.Now().UTC(),
+			FeedID:      feed.ID,
+		})
+		if err != nil {
+			if strings.Contains(err.Error(), "duplicate key") {
+				continue
+			}
+			log.Printf("Error inserting post: %s", err.Error())
+			continue
+		}
+
 	}
 	log.Printf("Found %v items", len(fetchedFeed.Channel.Item))
 }
