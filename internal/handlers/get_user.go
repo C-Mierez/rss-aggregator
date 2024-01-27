593fe96bb8ff96f14f6088d6a016e131a29e76d9
C-Mierez <carlos.mierez20@gmail.com>
2024-01-27 15:21:53 -0300

    ♻️ Removed auth logic to use middleware instead

--- internal/handlers/get_user.go
+++ internal/handlers/get_user.go
@@ -1,13 +1,11 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 
-	"github.com/c-mierez/rss-aggregator/internal/lib/auth"
 	"github.com/c-mierez/rss-aggregator/internal/lib/queries"
 	"github.com/c-mierez/rss-aggregator/internal/lib/serve"
-	"github.com/c-mierez/rss-aggregator/internal/store"
+	"github.com/c-mierez/rss-aggregator/internal/middleware"
 )
 
 type GetUserHandler struct {
@@ -31,17 +29,8 @@ type GetUserInput struct {
 }
 
 func (h *GetUserHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	apiKey, err := auth.GetAPIKey(r.Header)
-	if err != nil {
-		serve.JSONError(w, http.StatusUnauthorized, err.Error())
-		return
-	}
+	// Get the user data from the request context
+	authCTX := middleware.GetAuthCTX(r)
 
-	user, err := h.db.GetUserByAPIKey(r.Context(), apiKey)
-	if err != nil {
-		serve.JSONError(w, http.StatusBadRequest, fmt.Sprintf("Error fetching user: %v", err))
-		return
-	}
-
-	serve.JSONResponse(w, http.StatusOK, store.DBToStoreUser(user))
+	serve.JSONResponse(w, http.StatusOK, authCTX.User)
 }
